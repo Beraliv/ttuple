b5250f003b63160f874d4380481d991ba5bcaa30
Alexey Berezin <beraliv.spb@gmail.com>
2022-06-05 14:18:36 +0100

    feat: ➕ add StronglyTypedArray.prototype.map

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,17 @@ class StronglyTypedArray<T> {
   constructor(items: T[]) {
     this.#items = items;
   }
+
+  map<U>(
+    callback: (value: T, index: number, array: T[]) => U
+  ): StronglyTypedArray<U> {
+    // @ts-expect-error: T[] => U[]
+    this.#items = this.#items.map(callback);
+    // @ts-expect-error: StronglyTypedArray<T> => StronglyTypedArray<U>
+    return this;
+  }
 }
+
+[].map;
 
 export default StronglyTypedArray;
