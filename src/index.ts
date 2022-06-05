8a0d1d46b1526a0a0f3824c79c270705afba4588
Alexey Berezin <beraliv.spb@gmail.com>
2022-06-05 14:40:51 +0100

    feat: ➕ add map implementation 1

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,20 @@
-class StronglyTypedArray<T> {
-  #items: T[];
+type AnyArray = readonly any[];
+type Values<T extends AnyArray> = T[number];
 
-  constructor(items: T[]) {
+type Map<T, U> = any[] extends T
+  ? U[]
+  : T extends [any, ...infer Tail]
+  ? [U, ...Map<Tail, U>]
+  : [];
+
+class StronglyTypedArray<T extends AnyArray> {
+  #items: T;
+
+  constructor(items: T) {
     this.#items = items;
   }
 
-  map<U>(
-    callback: (value: T, index: number, array: T[]) => U
-  ): StronglyTypedArray<U> {
+  map<U>(callback: (value: Values<T>) => U): StronglyTypedArray<Map<T, U>> {
     // @ts-expect-error: T[] => U[]
     this.#items = this.#items.map(callback);
     // @ts-expect-error: StronglyTypedArray<T> => StronglyTypedArray<U>
