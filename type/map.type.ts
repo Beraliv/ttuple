08124dfff271b384b5333f92416337536a55294b
Alexey Berezin <beraliv.spb@gmail.com>
2022-06-07 16:22:08 +0100

    test: 🧪 map & filter: index

--- type/map.type.ts
+++ type/map.type.ts
@@ -9,11 +9,17 @@ declare const number3Tuple: [number, number, number];
 const staNumbers = new StronglyTypedArray(numbers);
 const staNumbersToStrings1 = staNumbers.map(String);
 const staNumbersToStrings2 = staNumbers.map((value) => value.toString());
+const staNumbersToStrings3 = staNumbers.map(
+  (value, index) => `${value}-${index}`
+);
 
 const staNumber1Tuple = new StronglyTypedArray(number1Tuple);
 const staNumber1TupleToString1Tuple1 = staNumber1Tuple.map(String);
 const staNumber1TupleToString1Tuple2 = staNumber1Tuple.map((value) =>
   value.toString()
+);
+const staNumber1TupleToString1Tuple3 = staNumber1Tuple.map(
+  (value, index) => `${value}-${index}`
 );
 
 const staNumber2Tuple = new StronglyTypedArray(number2Tuple);
@@ -21,11 +27,17 @@ const staNumber2TupleToString2Tuple1 = staNumber2Tuple.map(String);
 const staNumber2TupleToString2Tuple2 = staNumber2Tuple.map((value) =>
   value.toString()
 );
+const staNumber2TupleToString2Tuple3 = staNumber2Tuple.map(
+  (value, index) => `${value}-${index}`
+);
 
 const staNumber3Tuple = new StronglyTypedArray(number3Tuple);
 const staNumber3TupleToString3Tuple1 = staNumber3Tuple.map(String);
 const staNumber3TupleToString3Tuple2 = staNumber3Tuple.map((value) =>
   value.toString()
+);
+const staNumber3TupleToString3Tuple3 = staNumber3Tuple.map(
+  (value, index) => `${value}-${index}`
 );
 
 type cases = [
@@ -36,12 +48,18 @@ type cases = [
   AssertTrue<
     IsExact<typeof staNumbersToStrings2, StronglyTypedArray<string[]>>
   >,
+  AssertTrue<
+    IsExact<typeof staNumbersToStrings3, StronglyTypedArray<string[]>>
+  >,
   AssertTrue<IsExact<typeof staNumber1Tuple, StronglyTypedArray<[number]>>>,
   AssertTrue<
     IsExact<typeof staNumber1TupleToString1Tuple1, StronglyTypedArray<[string]>>
   >,
   AssertTrue<
     IsExact<typeof staNumber1TupleToString1Tuple2, StronglyTypedArray<[string]>>
+  >,
+  AssertTrue<
+    IsExact<typeof staNumber1TupleToString1Tuple3, StronglyTypedArray<[string]>>
   >,
   AssertTrue<
     IsExact<typeof staNumber2Tuple, StronglyTypedArray<[number, number]>>
@@ -60,6 +78,12 @@ type cases = [
   >,
   AssertTrue<
     IsExact<
+      typeof staNumber2TupleToString2Tuple3,
+      StronglyTypedArray<[string, string]>
+    >
+  >,
+  AssertTrue<
+    IsExact<
       typeof staNumber3Tuple,
       StronglyTypedArray<[number, number, number]>
     >
@@ -73,6 +97,12 @@ type cases = [
   AssertTrue<
     IsExact<
       typeof staNumber3TupleToString3Tuple2,
+      StronglyTypedArray<[string, string, string]>
+    >
+  >,
+  AssertTrue<
+    IsExact<
+      typeof staNumber3TupleToString3Tuple3,
       StronglyTypedArray<[string, string, string]>
     >
   >
