e7aca407077ceb67107851f441d43f4d8a236139
Alexey Berezin <beraliv.spb@gmail.com>
2022-06-12 19:27:44 +0100

    test: 🧪 map

--- type/map.type.ts
+++ type/map.type.ts
@@ -1,5 +1,5 @@
 import { AssertTrue, IsExact } from "conditional-type-checks";
-import sta, {StronglyTypedArray} from "../src";
+import sta, { StronglyTypedArray } from "../src";
 
 declare const numbers: number[];
 declare const number1Tuple: [number];
@@ -7,103 +7,56 @@ declare const number2Tuple: [number, number];
 declare const number3Tuple: [number, number, number];
 
 const staNumbers = sta(numbers);
-const staNumbersToStrings1 = staNumbers.map(String);
-const staNumbersToStrings2 = staNumbers.map((value) => value.toString());
-const staNumbersToStrings3 = staNumbers.map(
-  (value, index) => `${value}-${index}`
-);
+const numbersToStrings1 = staNumbers.map(String).toArray();
+const numbersToStrings2 = staNumbers.map((value) => value.toString()).toArray();
+const numbersToStrings3 = staNumbers
+  .map((value, index) => `${value}-${index}`)
+  .toArray();
 
 const staNumber1Tuple = sta(number1Tuple);
-const staNumber1TupleToString1Tuple1 = staNumber1Tuple.map(String);
-const staNumber1TupleToString1Tuple2 = staNumber1Tuple.map((value) =>
-  value.toString()
-);
-const staNumber1TupleToString1Tuple3 = staNumber1Tuple.map(
-  (value, index) => `${value}-${index}`
-);
+const number1TupleToString1Tuple1 = staNumber1Tuple.map(String).toArray();
+const number1TupleToString1Tuple2 = staNumber1Tuple
+  .map((value) => value.toString())
+  .toArray();
+const number1TupleToString1Tuple3 = staNumber1Tuple
+  .map((value, index) => `${value}-${index}`)
+  .toArray();
 
 const staNumber2Tuple = sta(number2Tuple);
-const staNumber2TupleToString2Tuple1 = staNumber2Tuple.map(String);
-const staNumber2TupleToString2Tuple2 = staNumber2Tuple.map((value) =>
-  value.toString()
-);
-const staNumber2TupleToString2Tuple3 = staNumber2Tuple.map(
-  (value, index) => `${value}-${index}`
-);
+const number2TupleToString2Tuple1 = staNumber2Tuple.map(String).toArray();
+const number2TupleToString2Tuple2 = staNumber2Tuple
+  .map((value) => value.toString())
+  .toArray();
+const number2TupleToString2Tuple3 = staNumber2Tuple
+  .map((value, index) => `${value}-${index}`)
+  .toArray();
 
 const staNumber3Tuple = sta(number3Tuple);
-const staNumber3TupleToString3Tuple1 = staNumber3Tuple.map(String);
-const staNumber3TupleToString3Tuple2 = staNumber3Tuple.map((value) =>
-  value.toString()
-);
-const staNumber3TupleToString3Tuple3 = staNumber3Tuple.map(
-  (value, index) => `${value}-${index}`
-);
+const number3TupleToString3Tuple1 = staNumber3Tuple.map(String).toArray();
+const number3TupleToString3Tuple2 = staNumber3Tuple
+  .map((value) => value.toString())
+  .toArray();
+const number3TupleToString3Tuple3 = staNumber3Tuple
+  .map((value, index) => `${value}-${index}`)
+  .toArray();
 
 type cases = [
-  AssertTrue<IsExact<typeof staNumbers, StronglyTypedArray<number[]>>>,
+  AssertTrue<IsExact<typeof numbersToStrings1, string[]>>,
+  AssertTrue<IsExact<typeof numbersToStrings2, string[]>>,
+  AssertTrue<IsExact<typeof numbersToStrings3, string[]>>,
+  AssertTrue<IsExact<typeof number1TupleToString1Tuple1, [string]>>,
+  AssertTrue<IsExact<typeof number1TupleToString1Tuple2, [string]>>,
+  AssertTrue<IsExact<typeof number1TupleToString1Tuple3, [string]>>,
+  AssertTrue<IsExact<typeof number2TupleToString2Tuple1, [string, string]>>,
+  AssertTrue<IsExact<typeof number2TupleToString2Tuple2, [string, string]>>,
+  AssertTrue<IsExact<typeof number2TupleToString2Tuple3, [string, string]>>,
   AssertTrue<
-    IsExact<typeof staNumbersToStrings1, StronglyTypedArray<string[]>>
+    IsExact<typeof number3TupleToString3Tuple1, [string, string, string]>
   >,
   AssertTrue<
-    IsExact<typeof staNumbersToStrings2, StronglyTypedArray<string[]>>
+    IsExact<typeof number3TupleToString3Tuple2, [string, string, string]>
   >,
   AssertTrue<
-    IsExact<typeof staNumbersToStrings3, StronglyTypedArray<string[]>>
-  >,
-  AssertTrue<IsExact<typeof staNumber1Tuple, StronglyTypedArray<[number]>>>,
-  AssertTrue<
-    IsExact<typeof staNumber1TupleToString1Tuple1, StronglyTypedArray<[string]>>
-  >,
-  AssertTrue<
-    IsExact<typeof staNumber1TupleToString1Tuple2, StronglyTypedArray<[string]>>
-  >,
-  AssertTrue<
-    IsExact<typeof staNumber1TupleToString1Tuple3, StronglyTypedArray<[string]>>
-  >,
-  AssertTrue<
-    IsExact<typeof staNumber2Tuple, StronglyTypedArray<[number, number]>>
-  >,
-  AssertTrue<
-    IsExact<
-      typeof staNumber2TupleToString2Tuple1,
-      StronglyTypedArray<[string, string]>
-    >
-  >,
-  AssertTrue<
-    IsExact<
-      typeof staNumber2TupleToString2Tuple2,
-      StronglyTypedArray<[string, string]>
-    >
-  >,
-  AssertTrue<
-    IsExact<
-      typeof staNumber2TupleToString2Tuple3,
-      StronglyTypedArray<[string, string]>
-    >
-  >,
-  AssertTrue<
-    IsExact<
-      typeof staNumber3Tuple,
-      StronglyTypedArray<[number, number, number]>
-    >
-  >,
-  AssertTrue<
-    IsExact<
-      typeof staNumber3TupleToString3Tuple1,
-      StronglyTypedArray<[string, string, string]>
-    >
-  >,
-  AssertTrue<
-    IsExact<
-      typeof staNumber3TupleToString3Tuple2,
-      StronglyTypedArray<[string, string, string]>
-    >
-  >,
-  AssertTrue<
-    IsExact<
-      typeof staNumber3TupleToString3Tuple3,
-      StronglyTypedArray<[string, string, string]>
-    >
+    IsExact<typeof number3TupleToString3Tuple3, [string, string, string]>
   >
 ];
