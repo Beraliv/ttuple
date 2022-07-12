9673424fdadf4eb9119e39f348cadbf693468c5d
Alexey Berezin <beraliv.spb@gmail.com>
2022-07-12 19:50:56 +0100

    test: 🧪 At

--- tests/types/At.type.ts
+++ tests/types/At.type.ts
@@ -3,29 +3,38 @@ import { At } from "../../src/types/At";
 
 type RA = readonly string[];
 type A = string[];
+type SAS = [string, ...A, string];
+type SSASS = [string, ...A, string];
 type T0 = [];
 type T1 = [string];
 type T2 = [string, string];
 type T3 = [string, string, string];
 
 type cases = [
-  // 1
-  AssertTrue<IsExact<At<RA, "1">, string | undefined>>,
-  AssertTrue<IsExact<At<A, "1">, string | undefined>>,
-  AssertTrue<IsExact<At<T0, "1">, undefined>>,
-  AssertTrue<IsExact<At<T1, "1">, undefined>>,
-  AssertTrue<IsExact<At<T2, "1">, string>>,
-  AssertTrue<IsExact<At<T3, "1">, string>>,
   // 2
   AssertTrue<IsExact<At<RA, "2">, string | undefined>>,
   AssertTrue<IsExact<At<A, "2">, string | undefined>>,
+  AssertTrue<IsExact<At<SAS, "2">, string | undefined>>,
+  AssertTrue<IsExact<At<SSASS, "2">, string | undefined>>,
   AssertTrue<IsExact<At<T0, "2">, undefined>>,
   AssertTrue<IsExact<At<T1, "2">, undefined>>,
   AssertTrue<IsExact<At<T2, "2">, undefined>>,
   AssertTrue<IsExact<At<T3, "2">, string>>,
+  // 1
+  AssertTrue<IsExact<At<RA, "1">, string | undefined>>,
+  AssertTrue<IsExact<At<A, "1">, string | undefined>>,
+  AssertTrue<IsExact<At<SAS, "1">, string | undefined>>,
+  // @ts-expect-error: expected `string` but got `string | undefined`
+  AssertTrue<IsExact<At<SSASS, "1">, string>>,
+  AssertTrue<IsExact<At<T0, "1">, undefined>>,
+  AssertTrue<IsExact<At<T1, "1">, undefined>>,
+  AssertTrue<IsExact<At<T2, "1">, string>>,
+  AssertTrue<IsExact<At<T3, "1">, string>>,
   // 0
   AssertTrue<IsExact<At<RA, "0">, string | undefined>>,
   AssertTrue<IsExact<At<A, "0">, string | undefined>>,
+  AssertTrue<IsExact<At<SAS, "0">, string>>,
+  AssertTrue<IsExact<At<SSASS, "0">, string>>,
   AssertTrue<IsExact<At<T0, "0">, undefined>>,
   AssertTrue<IsExact<At<T1, "0">, string>>,
   AssertTrue<IsExact<At<T2, "0">, string>>,
@@ -33,6 +42,8 @@ type cases = [
   // -1
   AssertTrue<IsExact<At<RA, "-1">, string | undefined>>,
   AssertTrue<IsExact<At<A, "-1">, string | undefined>>,
+  AssertTrue<IsExact<At<SAS, "-1">, string>>,
+  AssertTrue<IsExact<At<SSASS, "-1">, string>>,
   AssertTrue<IsExact<At<T0, "-1">, undefined>>,
   AssertTrue<IsExact<At<T1, "-1">, string>>,
   AssertTrue<IsExact<At<T2, "-1">, string>>,
@@ -40,6 +51,8 @@ type cases = [
   // -2
   AssertTrue<IsExact<At<RA, "-2">, string | undefined>>,
   AssertTrue<IsExact<At<A, "-2">, string | undefined>>,
+  AssertTrue<IsExact<At<SAS, "-2">, string | undefined>>,
+  AssertTrue<IsExact<At<SSASS, "-2">, string | undefined>>,
   AssertTrue<IsExact<At<T0, "-2">, undefined>>,
   AssertTrue<IsExact<At<T1, "-2">, undefined>>,
   AssertTrue<IsExact<At<T2, "-2">, string>>,
