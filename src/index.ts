ca73cbc4dff23aa39c97332758f8440e647a475d
Alexey Berezin <beraliv.spb@gmail.com>
2022-08-05 09:40:55 +0100

    refactor: ❌ remove ElementOf

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,9 @@
 import { ToTuple } from "./types/ToTuple";
-import { ElementOf } from "./types/ElementOf";
 import { AnyArray } from "./types/AnyArray";
 
-const length = <
-  T extends AnyArray,
-  S extends `${number}`,
-  R = ToTuple<ElementOf<T>, S>
->(
-  array: T,
+const length = <S extends `${number}`, R = ToTuple<S>>(
+  array: AnyArray,
   condition: `>= ${S}`
-): array is R extends T ? R : never => {
-  const expectedLength = Number(condition.split(" ")[1]);
-
-  if (array.length >= expectedLength) {
-    return true;
-  }
-
-  return false;
-};
+): array is R extends AnyArray ? R : never => true;
 
 export { length };
--- src/types/ElementOf.ts
+++
@@ -1,5 +0,0 @@
-import { AnyArray } from "./AnyArray";
-
-type ElementOf<T extends AnyArray> = T extends readonly (infer V)[] ? V : never;
-
-export type { ElementOf };
